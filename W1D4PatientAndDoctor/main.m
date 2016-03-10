e8cc6bcb4845fad3f6fec48ed7bace5954176990
Karlo Pagtakhan <kidap@yahoo.com>
2016-03-10 10:39:29 -0500

    Initial commit

---
+++ W1D4PatientAndDoctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  W1D4PatientAndDoctor
+//
+//  Created by Karlo Pagtakhan on 03/10/2016.
+//  Copyright © 2016 AccessIT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
